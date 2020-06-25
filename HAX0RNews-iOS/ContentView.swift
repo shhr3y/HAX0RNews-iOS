f59636a962bfabd9eb9f102e0cc959f2aa6e0c97
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-06-26 04:37:07 +0530

    Initial Commit

---
+++ HAX0RNews-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HAX0RNews-iOS
+//
+//  Created by Shrey on 26/06/20.
+//  Copyright © 2020 Shrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
