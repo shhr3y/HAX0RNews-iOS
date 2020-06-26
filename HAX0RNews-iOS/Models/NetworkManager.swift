bd8939da89b17f334d627a944d24e42e3950cec4
Shrey's Macbook Pro <shhr3y@gmail.com>
2020-06-27 05:24:39 +0530

    Completed

--- HAX0RNews-iOS/Models/NetworkManager.swift
+++ HAX0RNews-iOS/Models/NetworkManager.swift
@@ -14,7 +14,7 @@ class NetworkManager: ObservableObject {
     @Published var posts = [Post]()
     
     func fetchData(){
-        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page"){
+        if let url = URL(string: "httpsm://hn.algolia.com/api/v1/search?tags=front_page"){
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error == nil {
